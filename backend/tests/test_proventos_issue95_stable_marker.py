9c147ad9c00fc34124813eaeadd33caef71ab6ba
lfragoso93-web <lfragoso93@gmail.com>
2026-07-06 11:44:02 -0300

    chore: remove teste temporario de proventos

--- backend/tests/test_proventos_issue95_stable_marker.py
+++
@@ -1,9 +0,0 @@
-from datetime import date
-
-from app.services.proventos_service import _calc_net_qty
-from app.models.transaction import OperationType
-
-
-def test_issue95_calc_net_qty_ignora_compras_apos_data_com():
-    txs = [(date(2024, 1, 10), OperationType.buy, 100)]
-    assert _calc_net_qty(txs, date(2024, 1, 1)) == 0.0
